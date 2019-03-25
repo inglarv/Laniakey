4392ee7364148dec9b1f7660f27e2287faf8a747
hummingbirdwhocode <maksimmik21@gmail.com>
2019-03-25 17:06:10 +0200

    initial commit

--- src/PaymentGateService/RpcNodeConfiguration.cpp
+++ src/PaymentGateService/RpcNodeConfiguration.cpp
@@ -29,7 +29,7 @@ RpcNodeConfiguration::RpcNodeConfiguration() {
 void RpcNodeConfiguration::initOptions(boost::program_options::options_description& desc) {
   desc.add_options()
     ("daemon-address", po::value<std::string>()->default_value("localhost"), "laniakeyd address")
-    ("daemon-port", po::value<uint16_t>()->default_value(4810), "daemon port");
+    ("daemon-port", po::value<uint16_t>()->default_value(26080), "daemon port");
 }
 
 void RpcNodeConfiguration::init(const boost::program_options::variables_map& options) {
